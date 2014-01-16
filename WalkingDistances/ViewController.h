adbd85ccab3520dd09b93eddc5427e5c98da9f7a
JakobHartman <Jakob@Jakobs-MacBook-Pro-2.local>
2014-01-16 11:37:13 -0700

    Initial Commit

---
+++ WalkingDistances/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WalkingDistances
+//
+//  Created by Jakob Hartman on 1/16/14.
+//  Copyright (c) 2014 WhirlingPanguins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
